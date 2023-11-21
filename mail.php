02ac7243756bf867e06e9c55f7ff75861dd3e8d4
tadeumaffeis <tadeu.maffeis@gmail.com>
2023-11-21 14:36:15 -0300

    Email

--- mail.php
+++ mail.php
@@ -10,11 +10,12 @@ $mail = new PHPMailer(true);
 
 try {
     // Configurações do servidor de e-mail
-    $mail->isSMTP();
+    //$mail->isSMTP();
+    $mail->IsSendmail();
     $mail->Host = 'smtp.atmapps.pro.br';
     $mail->Port = 587; 			// A porta SMTP pode variar (587 é comum para TLS)
     //$mail->SMTPSecure = 'tls'; 	// Use 'tls' ou 'ssl' conforme necessário
-    $mail->SMTPAuth = true;
+    //$mail->SMTPAuth = true;
     $mail->Username = 'aaclassroom@atmapps.pro.br';
     $mail->Password = '@IA847atm';
 
