2a4726519c3aae321c39d734b384cb16facc40e5
tadeumaffeis <tadeu.maffeis@gmail.com>
2023-11-21 12:02:27 -0300

    Email

--- mail.php
+++ mail.php
@@ -1,7 +1,95 @@
 <?php
 
+$enviaFormularioParaNome = 'Antonio Tadeu Maffeis';
 
-echo mail("tadeu.maffeis@gmail.com", "Assunto", "Mensagem", "From: aaclassroom@atmapps.pro.br");
+$enviaFormularioParaEmail = 'tadeu.maffeis@gmail.com';
+
+
+$caixaPostalServidorNome = 'WebSite | Formulário';
+
+$caixaPostalServidorEmail = 'aaclassroom@atmapps.pro.br';
+
+$caixaPostalServidorSenha = 'IAatm874150631$';
+
+
+/*** FIM - DADOS A SEREM ALTERADOS DE ACORDO COM SUAS CONFIGURAÇÕES DE E-MAIL ***/
+
+
+/* abaixo as variaveis principais, que devem conter em seu formulario*/
+
+
+$remetenteNome  = "Antonio Tadeu Maffeis";
+
+$remetenteEmail = "aaclassroom@atmapps.pro.br";
+
+$assunto  = "Teste de e-mail";
+
+$mensagem = "<html><body><b>Teste</b></body></html>";
+
+
+$mensagemConcatenada = 'Formulário gerado via website'.'<br/>';
+
+$mensagemConcatenada .= '-------------------------------<br/><br/>';
+
+$mensagemConcatenada .= 'Nome: '.$remetenteNome.'<br/>';
+
+$mensagemConcatenada .= 'E-mail: '.$remetenteEmail.'<br/>';
+
+$mensagemConcatenada .= 'Assunto: '.$assunto.'<br/>';
+
+$mensagemConcatenada .= '-------------------------------<br/><br/>';
+
+$mensagemConcatenada .= 'Mensagem: "'.$mensagem.'"<br/>';
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+
+require 'classes/vendor/autoload.php';
+
+
+$mail = new PHPMailer();
+
+
+$mail->IsSMTP();
+
+$mail->SMTPAuth  = true;
+
+$mail->Charset   = 'utf8_decode()';
+
+$mail->Host  = 'smtp.'.substr(strstr($caixaPostalServidorEmail, '@'), 1);
+
+$mail->Port  = '587';
+
+$mail->Username  = $caixaPostalServidorEmail;
+
+$mail->Password  = $caixaPostalServidorSenha;
+
+$mail->From  = $caixaPostalServidorEmail;
+
+$mail->FromName  = utf8_decode($caixaPostalServidorNome);
+
+$mail->IsHTML(true);
+
+$mail->Subject  = utf8_decode($assunto);
+
+$mail->Body  = utf8_decode($mensagemConcatenada);
+
+
+$mail->AddAddress($enviaFormularioParaEmail,utf8_decode($enviaFormularioParaNome));
+
+
+if(!$mail->Send()){
+
+$mensagemRetorno = 'Erro ao enviar formulário: '. print($mail->ErrorInfo);
+
+}else{
+
+$mensagemRetorno = 'Formulário enviado com sucesso!';
+
+}
 
 die(0);
 
